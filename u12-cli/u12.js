90592059bd12aa48c0b164274d65a1a865ffc44a
duncap <gabriel_navarro@student.thesae.org>
2024-07-04 23:26:31 -0700

    Initial commit

---
+++ u12-cli/u12.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+
+if (process.argv.length < 5) {
+  console.error('Usage: ./u12.js <domain> <offer> <path-to-file>');
+  process.exit(1);
+}
+
+const [,, domain, offer, filePath] = process.argv;
+
+const submitOffer = async () => {
+  try {
+    const form = new FormData();
+    form.append('title', offer);
+    form.append('offerFile', fs.createReadStream(filePath));
+
+    const response = await axios.post(`http://${domain}/offer`, form, {
+      headers: {
+        ...form.getHeaders()
+      }
+    });
+
+    console.log(response.data.message);
+  } catch (error) {
+    console.error('Error submitting offer:', error.response ? error.response.data : error.message);
+  }
+};
+
+submitOffer();
